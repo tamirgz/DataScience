7cb3a27b9af02a483b98f0e447d60b1789153782
Tamir Gazneli <tamirgz@gmail.com>
2015-01-23 21:00:20 +0200

    Update the R script file

--- Getting_Clean_Data/Course_Project/run_analysis.R
+++ Getting_Clean_Data/Course_Project/run_analysis.R
@@ -9,10 +9,10 @@ yTrain = read.table("./UCI HAR Dataset/train/y_train.txt")
 subjectTest = read.table("./UCI HAR Dataset/test/subject_test.txt")
 subjectTrain = read.table("./UCI HAR Dataset/train/subject_train.txt")
 
-## add the activity type column
+## add the "activity type" column for each set
 xTestSet <- mutate(xTestSet,Y = yTest$V1)
 xTrainSet <- mutate(xTrainSet,Y = yTrain$V1)
-## add the subject ID column
+## add the "subject ID" column
 xTestSet <- mutate(xTestSet,Subject = subjectTest$V1)
 xTrainSet <- mutate(xTrainSet,Subject = subjectTrain$V1)
 ## remove the unnecessary variables (for simplicity)
@@ -28,31 +28,30 @@ q1Data <- arrange(mergedData,Subject)
 ## remove the unnecessary variables for simplicity
 rm("mergedData")
 
-## Read the features text file
+## Read the "features" text file
 temp <- scan("./UCI HAR Dataset/features.txt", what=list(NULL, name=character()))
 features = temp$name
 ## select the indexes of the variables that contain the word "mean()"
 means <- grep("mean()",features)
 ## select the indexes of the variables that contain the word "std()"
 stds <- grep("std()",features)
-## sort the indexes for simplicity
+## merge the indexes of features including "mean()" and "std()"
 selected <- c(means,stds)
+## sort the indexes for simplicity
 selected <- sort(selected)
 ## Select the "Subject","Activity" and columns including "mean()" and std()"
+#  This constructs the data set as requested in question 2 of the project
 q2Data <- q1Data[,c(1,2,selected+2)]
-#names(q2Data) <- c("Subject","Activity",features[selected])
+## write the q2Data to file: q2Data.txt
+write.table(q5Data,file = "./tidyData.txt")
+## set meaningful names for the first two columns
 colnames(q2Data)[1] <- "Subject"
 colnames(q2Data)[2] <- "Activity"
-## read the activities text file
-#temp <- scan("./UCI HAR Dataset/activity_labels.txt", what=list(NULL, name=character()))
-#activities <- temp$name
-## substitue the activity index with its name
-#q2Data$Activity <- activities[q2Data$Activity]
+
 ## remove the unnecessary variables for simplicity
-#rm("features"); rm("selected"); rm("temp"); rm("means"); rm("stds"); rm("activities")
 rm("features"); rm("selected"); rm("means"); rm("stds");
 
-## Calculate average for each variable, for each activity of each subject
+## Calculate average for each variable, for each subject and for each activity
 q5Data <- summarise_each(group_by(q2Data,Subject,Activity),funs(mean),3:ncol(q2Data))
 ## write the tidy table to file
 write.table(q5Data,file = "./tidyData.txt")
